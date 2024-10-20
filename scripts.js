c25a2f835a56f807191fac1cee2b74b602a87d0f
ExploreTutorialsOfficial <khushigandhi16211@gmail.com>
2024-10-20 17:47:48 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -72,3 +72,16 @@ window.addEventListener('scroll', function(){
     });
 
 
+    window.onscroll = function() {
+        let button = document.getElementById("goToTopBtn");
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            button.style.display = "block";
+        } else {
+            button.style.display = "none";
+        }
+    };
+    
+    function scrollToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+    
